984b2e11526d5e6b6a6e27ce044196a303b9193c
Faiz Mansuri <faizmansuri5@gmail.com>
2024-11-08 06:55:34 +0530

    1st

---
+++ script.js
@@ -0,0 +1,18 @@
+gsap.from("#img1", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+  y: 50,
+});
+gsap.from("#img2", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+  x: 50,
+});
+gsap.from("#img3", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+  y: -50,
+});
